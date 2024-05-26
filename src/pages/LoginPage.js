baafb549bb5338e3b77b0583e5e6e9f24fcb8d64
jin <jin200126@gmail.com>
2024-05-27 01:44:21 +0900

    basic login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import api from "../utils/api";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const [enterEmail, setEnterEmail] = useState('')
+  const [enterPassword, setEnterPassword] = useState('')
+  const [LoginError, setLoginError] = useState('')
+  const [user, setUser] = useState(null)
+
+  const navigate = useNavigate()
+
+  const AttemptLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const response = await api.post('/user/login', { email: enterEmail, password: enterPassword })
+      if (response.status == 200) {
+        console.log("login success!!")
+        setUser(response.data.user)
+        sessionStorage.setItem("token", response.data.token)
+        api.defaults.headers['authorization'] = "Bearer " + response.data.token
+        setLoginError('')
+        navigate('/')
+      } else {
+        throw new Error(response.message)
+      }
+    } catch (error) {
+      setLoginError(error.message)
+    }
+  }
+
   return (
     <div className="display-center">
-      <Form className="login-box">
+      {LoginError && <div className="error">{LoginError}</div>}
+      <Form className="login-box" onSubmit={AttemptLogin}>
         <h1>로그인</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={(event) => setEnterEmail(event.currentTarget.value)} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(event) => setEnterPassword(event.currentTarget.value)} />
         </Form.Group>
         <div className="button-box">
           <Button type="submit" className="button-primary">
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,30 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import api from '../utils/api'
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [secondPassword, setSecondPassword] = useState('')
+  const [error, setError] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    try {
+      if (password !== secondPassword) {
+        // 에러: 다시 입력한 패스워드 일치 안함
+        throw new Error("다시 입력한 패스워드가 일치하지 않습니다")
+      }
+      // api
+      const response = await api.post('/user', { name, email, password })
+      if (response.status == 200) {
+        navigate('/login')
+      } else {
+        throw new Error(response.data.error)
+      }
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
   return (
     <div className="display-center">
-      <Form className="login-box">
+      {error && <div className="error">{error}</div>}
+      <Form className="login-box" onSubmit={handleSubmit}>
         <h1>회원가입</h1>
         <Form.Group className="mb-3" controlId="formName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="string" placeholder="Name" />
+          <Form.Control type="string" placeholder="Name" onChange={(event) => setName(event.target.value)} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={(event) => setEmail(event.target.value)} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>re-enter the password</Form.Label>
-          <Form.Control type="password" placeholder="re-enter the password" />
+          <Form.Control type="password" placeholder="re-enter the password" onChange={(event) => setSecondPassword(event.target.value)} />
         </Form.Group>
 
         <Button className="button-primary" type="submit">
